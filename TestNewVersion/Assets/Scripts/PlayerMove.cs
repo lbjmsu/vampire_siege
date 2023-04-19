669afd6e482f90f3475a271d162cb23cb95effa2
lbjmsu <lbjohnson8@moreheadstate.edu>
2023-04-19 16:31:15 -0400

    Split Screen and Player Movement
    This is like a whole new game now

--- TestNewVersion/Assets/Scripts/PlayerMove.cs
+++ TestNewVersion/Assets/Scripts/PlayerMove.cs
@@ -6,22 +6,16 @@ public class PlayerMove : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public float speed = 1;
-    public float rotationSmoothness = 1;
     public float velocitySmoothness = 1;
-    public Camera mainCamera;
+    public float rotationSpeed = 1;
+    public float rotationSmoothness = 1;
+    public Camera playerCamera;
 
     [Header("Set dynamically")]
-    public Vector3 velocityVector;
-
-    private bool movingVertically = false;
-    private bool movingHorizontally = false;
+    public Vector3 movementDirection;
+    public float rotationAmount;
 
     private Rigidbody playerRB;
-
-    private float toRotateTo;
-    private float xRotation;
-    private float yRotation;
-    string directionString;
 
     private void Start()
     {
@@ -31,85 +25,104 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //  If the player is pressing a button...
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) ||
-            Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.DownArrow))
+        //  If player one is pressing a button...
+        if (gameObject.name == "PlayerOne")
         {
-            //  Gets input velocity
-            velocityVector.x = Input.GetAxis("Horizontal");
-            velocityVector.z = Input.GetAxis("Vertical");
-
-            //  Determine which direction the player is moving
-            CheckMovementOfPlayer();
-
-            //  Instantiate local variables for rotation
-            xRotation = velocityVector.x * 90;
-            yRotation = 180 - (velocityVector.z * 90 + 90);
-
-            //  If moving in the negative x direction, make sure that the rotation in the z direction is also negative
-            //      such that averaging between them is possible.
-            if (xRotation < 0)
-            {
-                yRotation *= -1;
-            }
-
-            //  If the player is moving both vertically and horizontally, average the rotation
-            if(movingVertically && movingHorizontally)
-            {
-                toRotateTo = (xRotation + yRotation) / 2;
-                /*directionString = "Diagonal";*/
-            }
-            //  If the player is only moving vertically, rotate to vertical rotation
-            else if (movingVertically)
-            {
-                toRotateTo = yRotation;
-                /*directionString = "Vertical";*/
-            }
-            //  If the player is only moving horizontally, rotate to horizontal rotation
-            else if (movingHorizontally)
-            {
-                toRotateTo = xRotation;
-                /*directionString = "Horizontal";*/
-            }
-            //  Otherwise, return because the player is not moving
-            else
-            {
-                return;
-            }
-
-            //  Reset movement variables
-            movingHorizontally = false;
-            movingVertically = false;
-
-            //  Debug
-            /*Debug.Log("Horizontal: " + xRotation + " | Vertical: " + yRotation +
-                " | Direction: " + toRotateTo + " | Moving " + directionString);*/
-
-            //  Rotate the player to this new rotation
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, toRotateTo, 0),
-                rotationSmoothness * Time.deltaTime * (Input.GetKey(KeyCode.DownArrow) ? 0.7f : 1));
+            MovePlayer();
         }
 
-        //  else, velocity vector lerp towards 0
-        else
+        //  If player two is pressing a button...
+        else if (gameObject.name == "PlayerTwo")
         {
-            velocityVector = Vector3.Lerp(velocityVector, Vector3.zero, velocitySmoothness * Time.deltaTime);
+            MovePlayer(false);
         }
 
-        //  Either way, move by velocity vector
-        playerRB.MovePosition(transform.position + velocityVector * speed * Time.deltaTime);
-        mainCamera.transform.position = transform.position + new Vector3(0, 17, -4);
+        //  Move the camera if player one
+        playerCamera.transform.position = transform.position + new Vector3(0, 17, -4);
     }
 
-    private void CheckMovementOfPlayer()
+    /// <summary>
+    ///     Allows for the user to move a certain player based on the playerOne boolean. "true" means that playerOne is being controlled.
+    ///     "false" means that playerTwo is being controlled.
+    /// </summary>
+    private void MovePlayer(bool playerOne = true)
     {
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        //      SECTION: Creation of the movement/rotation vectors
+
+        //  If player one or player two are moving forward...
+        if ((playerOne && Input.GetKey(KeyCode.W)) || (!playerOne && Input.GetKey(KeyCode.UpArrow)))
         {
-            movingVertically = true;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            //  Movement direction is in the direction the player is facing
+            movementDirection = new Vector3(Mathf.Cos(ToCartestian(transform.eulerAngles.y) * Mathf.Deg2Rad), 0,
+                Mathf.Sin(ToCartestian(transform.eulerAngles.y) * Mathf.Deg2Rad));
+
+            //  Normalize the vector so that its magnitude is one -- as such it only functions as a direction vector, and the
+            //      speed variable dictates its magnitude.
+            movementDirection.Normalize();
+        }    
+
+        //  If player one or player two are moving backward...
+        else if((playerOne && Input.GetKey(KeyCode.S)) || (!playerOne && Input.GetKey(KeyCode.DownArrow)))
         {
-            movingHorizontally = true;
+            //  Same thing as above, but backwards
+            movementDirection = new Vector3(Mathf.Cos(ToCartestian(transform.eulerAngles.y) * Mathf.Deg2Rad), 0,
+                Mathf.Sin(ToCartestian(transform.eulerAngles.y) * Mathf.Deg2Rad));
+
+            movementDirection.Normalize();
+
+            //  Makes the player do the opposite movement
+            movementDirection *= -1;
         }
+
+        //  Otherwise, movement direction lerps towards zero. Now it will affect the speed of the character.
+        else
+        {
+            movementDirection = Vector3.Lerp(movementDirection, Vector3.zero, velocitySmoothness);
+        }
+
+        //  If player one or two are turning left...
+        if ((playerOne && Input.GetKey(KeyCode.A)) || (!playerOne && Input.GetKey(KeyCode.LeftArrow)))
+        {
+            //  Player rotation amount is speed * 1 revolution/second clockwise
+            rotationAmount = rotationSpeed * 360 * Time.deltaTime * -1;
+        }
+
+        //  If player one or two are turning right...
+        else if ((playerOne && Input.GetKey(KeyCode.D)) || (!playerOne && Input.GetKey(KeyCode.RightArrow)))
+        {
+            //  Player rotation amount is speed * 1 revloution/second counter-clockwise
+            rotationAmount = rotationSpeed * 360 * Time.deltaTime;
+        }
+
+        //  If player one or two are not turning...
+        else
+        {
+            //  Rotation amount lerps towards zero to smoothly stop rotating.
+            rotationAmount = Mathf.Lerp(rotationAmount, 0, rotationSmoothness);
+        }
+
+        //      SECTION: Application of the movement/rotation vectors
+        
+        //  Move towards the movement direction
+        playerRB.MovePosition(transform.position + speed * Time.deltaTime * movementDirection);
+
+        //  Rotate the player to this new rotation
+        transform.Rotate(new Vector3(0, rotationAmount, 0));
+    }
+
+    /// <summary>
+    ///     Translates from Unity angles to Cartesian angles.
+    /// </summary>
+    private float ToCartestian(float inAngle)
+    {
+        return (inAngle * -1 + 450) % 360;
+    }
+
+    /// <summary>
+    ///     Translates from Cartesian angles to Unity angles.
+    /// </summary>
+    private float ToUnityAngle(float inAngle)
+    {
+        return ((inAngle + 90) % -1 - 180) * -1;
     }
 }
